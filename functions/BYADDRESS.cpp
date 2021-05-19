1f06f022356a67a15d09071967f2f40113c03077
sanchitPasricha <sanchit0229@gmail.com>
2021-05-20 03:06:54 +0530

    Add files via upload

---
+++ functions/BYADDRESS.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int swap(int *x, int *y){
+    
+    int temp;
+    temp=*x;
+    *x=*y;
+    *y=temp;
+    
+}
+
+int main(){
+    int num1=10,num2=15,sum;
+    swap(num1,num2);
+    cout<<num1<<endl<<num2;
+    
+    return 0;
+}
